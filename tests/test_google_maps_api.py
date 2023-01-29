3da02e82dd7c499fa40d81b4733a92418e92b3f3
Maxim S <poplers24@gmail.com>
2023-01-29 20:30:54 +0300

    Editing the class instance call in the test and adding the GET method

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -9,4 +9,9 @@ class Test_create_place():
     def test_create_new_place(self):
 
         print("\nМетод POST")
-        result_post: Response = Google_maps_api.create_new_place()
+        result_post = Google_maps_api.create_new_place()
+        check_post = result_post.json()
+        place_id = check_post.get("place_id")
+
+        print("\nМетод GET")
+        result_get = Google_maps_api.get_new_place(place_id)
--- utils/api.py
+++ utils/api.py
@@ -35,3 +35,15 @@ class Google_maps_api():
         print(result_post.text)
         return result_post
 
+    """Метод для проверки новой локации"""
+
+    @staticmethod
+    def get_new_place(place_id):
+
+        get_resource = "/maps/api/place/get/json"   # Ресурс метода Get
+        get_url = base_url + get_resource + key + "&place_id=" + place_id
+        print(get_url)
+        result_get = Http_methods.get(get_url)
+        print(result_get.text)
+        return result_get
+
