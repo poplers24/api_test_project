dc063c97aec0604ba399d9dc351f02ed8a8a14d2
Maxim S <poplers24@gmail.com>
2023-01-30 21:53:05 +0300

    Adding methods for checking the presence of required fields in the request response check_json_token and checking the value of the fields check_json_value, and adding them to the test steps

--- utils/checking.py
+++ utils/checking.py
@@ -1,5 +1,5 @@
 """Методы для проверки ответов запросов"""
-from requests import Response
+import json
 
 
 class Checking():
@@ -9,3 +9,18 @@ class Checking():
     def check_status_code(result, status_code):
         assert status_code == result.status_code, f"Провал! Статус код {result.status_code} не соответствует ожидаемому {status_code}"
         print("Успешно! Статус код = " + str(result.status_code))
+
+    """Метод для проверки наличия обязательных полей в ответе запроса"""
+    @staticmethod
+    def check_json_token(result, expected_value):
+        token = json.loads(result.text)
+        assert list(token) == expected_value, f"Поля в ответе {list(token)} не соответствуют ожидаемым {expected_value}"
+        print("Все поля присутствуют")
+
+    """Метод для проверки значений обязательных полей в ответе запроса"""
+    @staticmethod
+    def check_json_value(result, field_name, expected_value):
+        check = result.json()
+        check_info = check.get(field_name)
+        assert check_info == expected_value, f"Значение поля status - {check_info} не соответствует ожидаемому значению {expected_value}"
+        print(field_name + " верен!")
